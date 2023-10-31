abc620aec52337039253e98870e663ccc3c4cda2
Mohamed Elashri <mail@elashri.com>
2023-10-31 02:03:42 +0000

    Update setup.py to have the same version as `pyproject.toml`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='txm',
-    version='0.1',
+    version='0.1.0',
     packages=find_packages(),
     author='Mohamed Elashri',
     author_email='txm@elashri.com',
